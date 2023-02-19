35cc7f49d868e6e312909ae108d1953e97f36ca5
PickleSons <78266908+5Picklebarry@users.noreply.github.com>
2023-02-19 04:22:05 -0500

    Update and rename Loader.lua to Pickle-Hub-Loader.lua

---
+++ Pickle-Hub/Pickle-Hub-Loader.lua
@@ -0,0 +1,13 @@
+--[[
+This is the Pickle Hub Loader.
+
+Currently only 3 games supported.
+]]
+
+if game.PlaceId == 5827078246 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Coins-Hero-Simulator.lua")))()
+elseif game.PlaceId == 12400504665 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Hood-Arena.lua")))()
+    elseif game.PlaceId == 662417684 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Lucky-Blocks-Battlegrounds.lua")))()
+end
