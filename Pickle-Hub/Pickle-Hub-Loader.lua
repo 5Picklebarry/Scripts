8866abd461bb4483a1334051e9ab365a50618f5c
PickleSons <78266908+5Picklebarry@users.noreply.github.com>
2023-03-26 16:53:22 -0400

    Update Pickle-Hub-Loader.lua

--- Pickle-Hub/Pickle-Hub-Loader.lua
+++ Pickle-Hub/Pickle-Hub-Loader.lua
@@ -11,7 +11,9 @@ elseif game.PlaceId == 12400504665 then
     elseif game.PlaceId == 662417684 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Lucky-Blocks-Battlegrounds.lua")))()
     elseif game.PlaceId == 10704789056 then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Drive-World")))()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Drive-World.lua")))()
+    elseif game.PlaceId == 11746859781 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Bubble-Gum-Clicker.lua")))()
     else
     warn("Pickle Hub is not supported in this game.")
 end
